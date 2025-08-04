5e34330a925ff19328b59d2988ed7cb766fe5abd
vailosman888 <ahmed.zayra1@gmail.com>
2025-08-04 08:30:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import os
 from datetime import datetime, timedelta
 from flask import Flask
 
@@ -82,7 +83,8 @@ def webhook_trigger():
         return f"❌ خطأ: {e}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
